640d23fd0a6c4cda41f92318f87506f9c63838d8
unknown <thiespetersen01@hotmail.com>
2025-08-11 12:16:55 +0200

    message

--- screen_box.gd
+++
@@ -1,6 +0,0 @@
-extends MeshInstance3D
-
-func _ready() -> void:
-	var box := mesh as BoxMesh
-	box.size.x = get_viewport().size.x
-	box.size.y = get_viewport().size.y
--- search_area.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-# signal clicked
-
-func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if event.is_action_pressed("Left Click"):
-		print("click?")
-		# clicked.emit()
