2c5f09ce930319086d344651f652b13e3d78d872
ani <anika.barop@hotmail.de>
2025-08-21 14:26:42 +0200

    context menu position logic

---
+++ Scenes/context_menu.gd
@@ -0,0 +1,62 @@
+@tool
+extends PanelContainer
+class_name ContextMenu
+
+enum ContextMenuTypes {
+	ITEM,
+	BACKGROUND,
+	TASKBAR,
+}
+
+const CONTEXT_MENU_MARGIN_X = 2
+const CONTEXT_MENU_MARGIN_Y = 1
+
+@export var context_menu_type: ContextMenuTypes :
+	set(value):
+		context_menu_type = value
+		_set_menu_content()
+
+
+func _ready() -> void:
+	add_to_group("ContextMenu")
+	_set_menu_content()
+
+
+func _set_menu_content() -> void:
+	for child in $MarginContainer.get_children():
+		child.queue_free()
+	if context_menu_type == ContextMenuTypes.BACKGROUND:
+		var background := preload("res://Scenes/ContextMenu/background.tscn").instantiate()
+		$MarginContainer.add_child(background)
+	elif context_menu_type == ContextMenuTypes.ITEM:
+		var item := preload("res://Scenes/ContextMenu/item.tscn").instantiate()
+		$MarginContainer.add_child(item)
+	elif context_menu_type == ContextMenuTypes.TASKBAR:
+		var taskbar := preload("res://Scenes/ContextMenu/taskbar.tscn").instantiate()
+		$MarginContainer.add_child(taskbar)
+
+
+func place_context_menu(event: InputEventMouseButton) -> void:
+	var screen_size := get_viewport_rect().size
+	position = event.position
+	if context_menu_type == ContextMenuTypes.TASKBAR:
+		if position.x + size.x > screen_size.x:
+			position.x = screen_size.x - size.x - CONTEXT_MENU_MARGIN_X
+		position.y = -CONTEXT_MENU_MARGIN_Y - size.y
+	elif context_menu_type == ContextMenuTypes.BACKGROUND:
+		if position.x + size.x > screen_size.x:
+			position.x = event.position.x - size.x
+		if position.y + size.y > screen_size.y:
+			position.y = event.position.y - size.y
+	for item in get_tree().get_nodes_in_group("ContextMenu"):
+		var control_node := item as Control
+		if control_node.visible:
+			control_node.visible = false
+	visible = true
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		var mouse_event := event as InputEventMouseButton
+		if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
+			visible = false
--- Scripts/desktop_wallpaper.gd
+++ Scripts/desktop_wallpaper.gd
@@ -1,18 +1,18 @@
 extends TextureRect
 
-@onready var context_menu := $ContextMenu as PanelContainer
+@onready var context_menu := $ContextMenu as ContextMenu
 
 
 func _ready() -> void:
 	context_menu.hide()
 
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action("Right Click"):
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
 		var mouse_event := event as InputEventMouseButton
-
-		context_menu.position = mouse_event.position
-		context_menu.visible = true
-
-	if event.is_action("Left Click"):
-		context_menu.visible = false
+		if mouse_event.button_index == MOUSE_BUTTON_RIGHT and mouse_event.pressed:
+			context_menu.place_context_menu(event)
+		#var mouse_event := event as InputEventMouseButton
+#
+		#context_menu.position = mouse_event.position
+		#context_menu.visible = true
