ede0bbc74e726bca74bbf7c313c3ca0377fda12e
DizTizz <thiespetersen01@hotmail.com>
2025-09-11 14:14:21 +0200

    Icon can now call on window
    Window Prototype Idea omitted, defaulted to using basic godot window node. Can use as child of icon node, move position to screen when needed etc.

---
+++ Scripts/myfiles.gd
@@ -0,0 +1,12 @@
+@tool
+extends Icon
+
+func _on_button_pressed() -> void:
+	if Gamemaster.click_icon.has(str(label.text)):
+		Gamemaster.click_icon[str(label.text)] += 1
+		print(Gamemaster.click_icon)
+	else:
+		Gamemaster.click_icon[str(label.text)] = 1
+		print(Gamemaster.click_icon)
+	$Window.position = get_global_mouse_position()
+	
