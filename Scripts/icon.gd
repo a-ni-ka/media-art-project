eb06fd8576bb06eb09118562002c84a5b1624f26
DizTizz <thiespetersen01@hotmail.com>
2025-09-11 14:08:13 +0200

    Icon Reworked
    Omitted Mouse as a 2D node (It overcomplicated wayyy to much). As such adapted icon to be simpler. To change code for your itteration of an icon, just copy one, right click, extend script.

--- Scripts/icon.gd
+++ Scripts/icon.gd
@@ -13,23 +13,21 @@ extends Area2D
 
 # When copying this icon, remember to connect to the mouse signal
 
-@onready var mouse_state = false
 @onready var label: Label = $Label
 
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Mouse":
-		mouse_state = true
+func _ready() -> void:
+	$ColorRect.visible = false
 
-func _on_body_exited(body: Node2D) -> void:
-	if body.name == "Mouse":
-		mouse_state = false
+func _on_button_pressed() -> void:
+	if Gamemaster.click_icon.has(str(label.text)):
+		Gamemaster.click_icon[str(label.text)] += 1
+		print(Gamemaster.click_icon)
+	else:
+		Gamemaster.click_icon[str(label.text)] = 1
+		print(Gamemaster.click_icon)
 
-func _on_mouse_mouse_pressed() -> void:
-	if mouse_state:
-		# write in your code for what the icon should do
-		if Gamemaster.click_icon.has(str(label.text)):
-			Gamemaster.click_icon[str(label.text)] += 1
-			print(Gamemaster.click_icon)
-		else:
-			Gamemaster.click_icon[str(label.text)] = 1
-			print(Gamemaster.click_icon)
+func _on_button_mouse_entered() -> void:
+	$ColorRect.visible = true
+
+func _on_button_mouse_exited() -> void:
+	$ColorRect.visible = false
