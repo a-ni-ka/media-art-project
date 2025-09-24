9443b25b64652219c81d71fc4ea733fca3a156a2
DizTizz <thiespetersen01@hotmail.com>
2025-09-24 10:17:52 +0200

    Minor Changes

--- Scripts/desktop_wallpaper.gd
+++ Scripts/desktop_wallpaper.gd
@@ -14,8 +14,8 @@ func _gui_input(event: InputEvent) -> void:
 		if mouse_event.button_index == MOUSE_BUTTON_RIGHT and mouse_event.pressed:
 			context_menu.place_context_menu(event)
 
-# Code additions made by Thies
 
+# Code additions made by Thies
 var bird = preload("res://Scenes/File_Window/bird.tscn")
 func _on_file_window_change_wallpaper(path: Variant) -> void:
 	texture = load(path)
@@ -28,9 +28,9 @@ func _on_file_window_bird_up() -> void:
 			var obj = bird.instantiate()
 			add_child(obj)
 			if obj.direction < 0:
-				obj.global_position = Vector2(randi_range(1700, 2200), randi_range(20,700))
+				obj.global_position = Vector2(randi_range(1700, 2700), randi_range(20,800))
 			else:
-				obj.global_position = Vector2(randi_range(-100, -600), randi_range(20,700))
+				obj.global_position = Vector2(randi_range(-100, -1100), randi_range(20,800))
 
 
 func _on_search_line_edit_text_submitted(new_text: String) -> void:
